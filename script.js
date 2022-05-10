79bc156ce15c88c607800dfa7a1393e17f3c75b7
Ashutosh <ashutoshtyagi1062@gmail.com>
2022-05-10 23:22:41 +0530

    quiz created

---
+++ script.js
@@ -0,0 +1,85 @@
+const question = [{
+        id: 0,
+        q: "What is capital of India?",
+        a: [{ text: "Gandhinagar", isCorrect: false },
+            { text: "Surat", isCorrect: false },
+            { text: "Delhi", isCorrect: true },
+            { text: "Mumbai", isCorrect: false }
+        ]
+  
+    },
+    {
+        id: 1,
+        q: "What is the capital of Thailand?",
+        a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+            { text: "Phuket", isCorrect: false },
+            { text: "Ayutthaya", isCorrect: false },
+            { text: "Bangkok", isCorrect: true }
+        ]
+  
+    },
+    {
+        id: 2,
+        q: "What is the capital of Gujarat",
+        a: [{ text: "Surat", isCorrect: false },
+            { text: "Vadodara", isCorrect: false },
+            { text: "Gandhinagar", isCorrect: true },
+            { text: "Rajkot", isCorrect: false }
+        ]
+  
+    }
+  
+]
+
+id=-1;
+
+function next()
+{
+  
+  if(id<2)
+  id=id+1;
+  if(id<3&&id>=0)
+  {fun();
+  var ele = document.getElementsByName("opt");
+  for(var i=0;i<ele.length;i++)
+   ele[i].checked = false;}
+   
+   else
+   {}
+
+  
+
+
+}
+function prev()
+{  if(id>0)
+  {id=id-1;
+  fun();}
+}
+
+
+function fun()
+{ 
+      if(id>=0&&id<2)
+    {
+      document.getElementById("start").innerHTML="next";
+      document.getElementById("prev").innerHTML="prev";
+    }
+    if(id==2){
+      document.getElementById("start").innerHTML="save and submit";
+      document.getElementById("prev").innerHTML="prev";
+    }
+
+  const ques=document.getElementById("ques");
+  const opt1=document.getElementById("opt1");
+  const opt2=document.getElementById("opt2");
+  const opt3=document.getElementById("opt3");
+  const opt4=document.getElementById("opt4");
+  ques.innerHTML=question[id].q;
+  opt1.innerHTML=question[id].a[0].text
+  opt2.innerHTML=question[id].a[1].text
+  opt3.innerHTML=question[id].a[2].text
+  opt4.innerHTML=question[id].a[3].text
+  
+
+}
